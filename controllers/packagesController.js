1f5d5e63cb7f9d236a009289e5da529035a0bcee
Xsozm <hazem29778865@gmail.com>
2020-01-04 17:46:12 +0100

    Controller

---
+++ controllers/packagesController.js
@@ -0,0 +1,5 @@
+export const calculateCost = (req, res) => {
+  const body = req.body; //input
+  for (let key in req.body) {
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { calculateCost } from '../controllers/packagesController';
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.send('Hello World');
 });
+
+router.post('/calculateCost', calculateCost);
 
 export default router;
