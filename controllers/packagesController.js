bc61d790eefd6e8151a2f9f0c52c326773a0c0af
Xsozm <hazem29778865@gmail.com>
2020-01-04 21:00:29 +0100

    AddDynamicProgrammingHelperMethod

--- controllers/packagesController.js
+++ controllers/packagesController.js
@@ -1,13 +1,28 @@
 import { getOptimalCost } from '../helpers/packsHelper';
+import prodcuts from '../data/items';
 export const calculateCost = (req, res) => {
   const body = req.body; //input
   const orders = [];
   for (let packCode in body) {
-    const { totalCost, packs, error } = getOptimalCost(
-      packCode,
-      body[packCode]
+    const packsLimits = prodcuts[packCode].Packs.map(pack => pack.limit);
+    const packsPrices = prodcuts[packCode].Packs.map(pack => pack.price);
+
+    const targetValue = body[packCode];
+    const { totalNumberOfPacks, optimalPacks, totalCost } = getOptimalCost(
+      packsLimits,
+      targetValue,
+      packsPrices
     );
-    orders.push({ status, totalCost, packs, error });
+
+    //console.log(totalNumberOfPacks);
+    console.log(optimalPacks);
+    console.log(totalCost);
+
+    // const { totalCost, packs, error } = getOptimalCost(
+    //   packCode,
+    //   body[packCode]
+    // );
+    // orders.push({ status, totalCost, packs, error });
   }
   const responseObject = {
     Orders: orders,
--- helpers/packsHelper.js
+++ helpers/packsHelper.js
@@ -1,1 +1,50 @@
-export const getOptimalCost = () => {};
+// Dynamic Programming Solution with CompleoptimalPacksity O(mV)
+const POSITIVE_INF = 10000000;
+export const getOptimalCost = (packs, v, prices) => {
+  const Memo = [];
+  const table = [];
+  const packsUsed = [];
+  table[0] = 0;
+  for (let i = 1; i <= v; i++) {
+    table[i] = POSITIVE_INF;
+  }
+  for (let i = 0; i < packs.length; i++) {
+    packsUsed[packs[i]] = 0;
+  }
+
+  for (let i = 1; i <= v; i++) {
+    // Go through all packs smaller than i
+    for (let j = 0; j < packs.length; j++)
+      if (packs[j] <= i) {
+        let sub_res = table[i - packs[j]];
+        if (sub_res !== POSITIVE_INF && sub_res + 1 < table[i]) {
+          table[i] = sub_res + 1;
+          Memo[i] = packs[j];
+        }
+      }
+  }
+  let a = v;
+  while (a > 0) {
+    packsUsed[Memo[a]]++;
+    a -= Memo[a];
+  }
+  let totalCost = 0;
+  const optimalPacks = [];
+  for (let i = 0; i < packs.length; i++) {
+    if (packsUsed[packs[i]] > 0) {
+      optimalPacks.push({
+        pack: packs[i],
+        used: packsUsed[packs[i]],
+        pricePerPack: prices[i],
+      });
+      totalCost += packsUsed[packs[i]] * prices[i];
+    }
+  }
+  totalCost;
+
+  return {
+    totalNumberOfPacks: table[v],
+    optimalPacks,
+    totalCost: Math.round(totalCost * 100) / 100,
+  };
+};
