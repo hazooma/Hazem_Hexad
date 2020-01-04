2f9e7413104213d70fb5c7677d90e7f33650ecc7
Xsozm <hazem29778865@gmail.com>
2020-01-04 18:15:08 +0100

    AddToController

--- controllers/packagesController.js
+++ controllers/packagesController.js
@@ -1,11 +1,16 @@
 import { getOptimalCost } from '../helpers/packsHelper';
 export const calculateCost = (req, res) => {
   const body = req.body; //input
+  const orders = [];
   for (let packCode in body) {
-    const { totalCost, packs } = getOptimalCost(packCode, body[packCode]);
+    const { totalCost, packs, error } = getOptimalCost(
+      packCode,
+      body[packCode]
+    );
+    orders.push({ status, totalCost, packs, error });
   }
   const responseObject = {
-    status: 'Success!',
+    Orders: orders,
   };
   res.status(200).json(responseObject);
 };
