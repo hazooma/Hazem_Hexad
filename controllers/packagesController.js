388740602775fec51bc42dbe1e1c8cf44df0aa2b
Xsozm <hazem29778865@gmail.com>
2020-01-04 17:52:32 +0100

    ControllerWorks

--- controllers/packagesController.js
+++ controllers/packagesController.js
@@ -1,5 +1,10 @@
 export const calculateCost = (req, res) => {
   const body = req.body; //input
-  for (let key in req.body) {
+  for (let packCode in body) {
+    const { totalCost, packs } = getOptimalCost(packCode, body[packCode]);
   }
+  const responseObject = {
+    status: 'Success!',
+  };
+  res.status(200).json(responseObject);
 };
